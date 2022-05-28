c822b7240e01f46440b6d11d3849f60fa7b98def
Sage Liem <sage.liem.9@gmail.com>
2022-05-28 14:40:17 -0400

    printmoves() partially implemented (name will probably be changed)
    Returns a linked list of gamestate structs consisting of all possible moves for a given piece. Incomplete

--- checkmove.c
+++ checkmove.c
@@ -1,15 +1,18 @@
 // checkmove.c
 //
-// Intended: Checks whether a move is valid.
-// Current functionality: returns result of moving a piece in a direction determined by an integer from 0 to 3
 
 #include <stdio.h>
+#include <string.h>
 #include "board.h"
 
 struct gamestate {
         int board[8][8];
         int turn;
         int score;
+	int past_x;
+	int past_y;
+	int curr_x;
+	int curr_y;
 };
 
 struct Space {
@@ -17,239 +20,180 @@ struct Space {
 	int y;
 };
 
-struct Space *convertnotation(int sp) {
-	return NULL;
+struct LinkedList {
+	struct gamestate *move;
+	struct LinkedList *next;
+};
+
+void append(struct LinkedList *list, struct gamestate *new) {
+	while (list->next) {
+		list = list->next;
+	}
+	list->next = malloc(sizeof(struct LinkedList));
+	list->next->move = new;
+	list->next->next = NULL;
 }
 
-struct Space *movediagonal(struct Space *pos, int dir) {
-	// 0 = +x +y
-	// 1 = -x +y
-	// 2 = +x -y
-	// 3 = -x -y
-	/*
-	int mv;
-	
-	if (space<4 && dir>=2) { // cannot move backwards from first row
-		return -1;
-	} else if (space >=29 && dir<=1) { // cannot move forward from last row
-		return -1;
-	} else if (dir == 0) {
-		mv = 4;
-	} else if (dir == 1) {
-		mv = 5;
-	} else if (dir == 2) {
-		mv = -3;
-	} else { // if dir == 3
-		mv = -4;
-	}
-		
-	if (space%8 > 4 || space%8 == 0) {
-		mv--;
-	}
-	printf("Move size: %d\n", mv);
+void printcaptures(struct gamestate *board_struct, int x, int y, int ydir, bool king, struct LinkedList *move_list) {
+	// TODO implement kings	
+	printf("Print captures\n");
+	if ((x>1 && (y+2*ydir>=0 && y+2*ydir<=8)) && ((board_struct->board[y+ydir][x-1] == XCHECK) || (board_struct->board[y+ydir][x-1] == XKING))) {
+		if (board_struct->board[y+2*ydir][x-2] == 0) {
+			printf("%d, %d\n", x-2, y+2*ydir);
+			struct gamestate *new_move = malloc(sizeof(struct gamestate));
+			memcpy(new_move, board_struct, sizeof(struct gamestate));
+			new_move->board[y+2*ydir][x-2] = new_move->board[y][x];
+			new_move->board[y+ydir][x-1] = 0;
+			new_move->board[y][x] = 0;
+			new_move->curr_x = x-2;
+			new_move->curr_y = y+2*ydir;
 
-	// prevent moving across board
-	int dest = space + mv;
-
-	if ((space%8==5) && (dest%8==0)) {
-		return -1;
-	} else if (dir == 0 || dir == 2) {
-		if (dest==12 || dest==20 || dest==28) {
-			return -1;
+			append(move_list, new_move);
+			
+			printcaptures(board_struct, x-2, y+2*ydir, ydir, king, move_list);
 		}
-	} else if (dir == 1 || dir == 3) {
-		if (dest==5 || dest==13 || dest==21 || dest==29) {
-			return -1;
+	}
+	if ((x<6 && (y+2*ydir>=0 && y+2*ydir<=8)) && ((board_struct->board[y+ydir][x+1] == XCHECK) || (board_struct->board[y+ydir][x+1] == XKING))) {
+		if (board_struct->board[y+2*ydir][x+2] == 0) {
+			printf("%d, %d\n", x+2, y+2*ydir);
+			printcaptures(board_struct, x+2, y+2*ydir, ydir, king, move_list);
 		}
 	}
 
-	return dest;
-	*/
-
-	int x = pos->x;
-	int y = pos->y;
-
-	if (((y==0) && (dir>=2)) ||
-		       ((y==7) && (dir<=1)) ||
-	       ((x==0) && ((dir==1) || (dir==3))) ||
-		((x==7 && ((dir==0) || (dir==2))))) {
-		return NULL;
+	if (king) {
+		printf("King captures");
+		if (x>1 && ((board_struct->board[y-ydir][x-1] == XCHECK) || (board_struct->board[y-ydir][x-1] == XKING))) {
+			if (board_struct->board[y-2*ydir][x-2] == 0) {
+				printf("%d, %d\n", x-2, y-2*ydir);
+			}
+		}
+		if (x<6 && ((board_struct->board[y-ydir][x+1] == XCHECK) || (board_struct->board[y-ydir][x+1] == XKING))) {
+			if (board_struct->board[y-2*ydir][x+2] == 0) {
+				printf("%d, %d\n", x-2, y-2*ydir);
+			}
+		}
 	}
-
-	struct Space *ptr = (struct Space*) malloc(sizeof(struct Space));
-	if (dir==0) {
-		x++;
-		y++;
-	} else if (dir==1) {
-		x++;
-		y--;
-	} else if (dir==2) {
-		x--;
-		y++;
-	} else if (dir==3) {
-		x--;
-		y--;
-	}
-
-	ptr->x = x;
-	ptr->y = y;
-
-	return ptr;
 }
 
+struct LinkedList *printmoves(struct gamestate *board_struct, int x, int y) {
+	struct LinkedList *move_list = malloc(sizeof(struct LinkedList));
+	move_list->next = NULL;
+	move_list->move = NULL;
 
-int printcaptures(struct gamestate *board_struct, struct Space *pos, player, piece_type) {
-	printf("TODO: implement captures");
-	return 0;
-}
+	int ydir;
+	int king;
+	int type = board_struct->board[y][x];
 
-
-
-int printmoves(struct gamestate *board_struct, struct Space *pos, player) {
-// int printmoves(int player) {
-	int mych;
-	int myking;
-	int oppch;
-	int oppking;
-	int forwardl;
-	int forwardr;
-	int backl;
-	int backr;
-	if (player == 2) {
-		mych = CHECK;
-		myking = KING;
-		oppch = XCHECK;
-		oppking = XKING;
-		forwardl = 3;
-		forwardr = 2;
-		backl = 1;
-		backr = 0;
+	if (type == CHECK) {
+		printf("Checker\n");
+		ydir = -1;
+		king = false;
+	} else if (type == KING) {
+		printf("King\n");
+		ydir = -1;
+		king = true;
+	} else if (type == XCHECK) {
+		printf("Enemy checker\n");
+		ydir = 1;
+		king = false;
+	} else if (type == XKING) {
+		printf("Enemy king\n");
+		ydir = 1;
+		king = true;
 	} else {
-		mych = XCHECK;
-		myking = XKING;
-		oppch = CHECK;
-		oppking = KING;
-		forwardl = 0;
-		forwardr = 1;
-		backl = 2;
-		backr = 3;
+		printf("No checker here!\n");
+		exit(1);
 	}
-
-	struct Space testspace;
-	testspace.x = 4;
-	testspace.y = 5;
-	
-	printf("Tests\n");
-	printf("Move 26 up left: %d, %d\n", movediagonal(&testspace, 2)->x, movediagonal(&testspace, 2)->y);
-	/* printf("Move 6 down left: %d\n", movediagonal(6, 0));
-	printf("Try to move backwards from first: %d\n", movediagonal(2, 3));
-	printf("Try to move left off board_struct->board: %d\n", movediagonal(13, 0));
-	printf("Try to move forwards from last: %d\n", movediagonal(30, 1));
-	*/
-
-	int piece = board_struct->board[pos->x][pos->y];
-	struct Space *new_space = (struct Space*) malloc(sizeof(struct Space));
-	printf("Moves:\n");
-	if (piece == mych) {
-		new_space = movediagonal(pos, forwardl);
-		int new_piece = board_struct->board[new_space->x][new_space->y];
-		if (new_piece == 0) {
-			printf("%d\n", forwardl);
-		} else if ((new_piece==oppch) || (new_piece==oppking)) {
-			printcaptures(board_struct, pos, player, piece);
+	printf("Ydir: %d\n", ydir);
+	if (((y+ydir) >= 0) && ((y+ydir) <= 7)) {
+		if ((x > 0) && (board_struct->board[y+ydir][x-1] == 0)) {
+			printf("Left: %d, %d\n", x-1, y+ydir);
+			struct gamestate *new_move = malloc(sizeof(struct gamestate));
+			memcpy(new_move, board_struct, sizeof(struct gamestate));
+			new_move->board[y+ydir][x-1] = new_move->board[y][x];
+			new_move->board[y][x] = 0;
+			new_move->curr_x = x-1;
+			new_move->curr_y = y+ydir;
+			append(move_list, new_move);
 		}
-
-		new_space = movediagonal(pos, forwardr);
-		new_piece = board_struct->board[new_space->x][new_space->y];
-		if (new_piece == 0) {
-			printf("%d\n", forwardr);
-		} else if ((new_piece==oppch) || (new_piece==oppking)) {
-			printcaptures(board_struct, pos, player, piece);
-		}
-	} else if (piece == myking) {
-		new_space = movediagonal(pos, forwardl);
-		int new_piece = board_struct->board[new_space->x][new_space->y];
-		if (new_piece == 0) {
-			printf("%d\n", forwardl);
-		} else if ((new_piece==oppch) || (new_piece==oppking)) {
-			printcaptures(board_struct, pos, player, piece);
-		}
-
-		new_space = movediagonal(pos, forwardr);
-		new_piece = board_struct->board[new_space->x][new_space->y];
-		if (new_piece == 0) {
-			printf("%d\n", forwardr);
-		} else if ((new_piece==oppch) || (new_piece==oppking)) {
-			printcaptures(board_struct, pos, player, piece);
-		}
-		
-		new_space = movediagonal(pos, backl);
-		new_piece = board_struct->board[new_space->x][new_space->y];
-		if (new_piece == 0) {
-			printf("%d\n", backl);
-		} else if ((new_piece==oppch) || (new_piece==oppking)) {
-			printcaptures(board_struct, pos, player, piece);
-		}
-
-		new_space = movediagonal(pos, backr);
-		new_piece = board_struct->board[new_space->x][new_space->y];
-		if (new_piece == 0) {
-			printf("%d\n", backr);
-		} else if ((new_piece==oppch) || (new_piece==oppking)) {
-			printcaptures(board_struct, pos, player, piece);
+		if ((x < 7) && (board_struct->board[y+ydir][x+1] == 0)) {
+			printf("Right: %d, %d\n", x+1, y+ydir);
+			struct gamestate *new_move = malloc(sizeof(struct gamestate));
+			memcpy(new_move, board_struct, sizeof(struct gamestate));
+			new_move->board[y+ydir][x+1] = new_move->board[y][x];
+			new_move->board[y][x] = 0;
+			new_move->curr_x = x+1;
+			new_move->curr_y = y+ydir;
+			append(move_list, new_move);
 		}
 	}
 	
-	return 0;
+	if (king) {
+		if (((y-ydir) >= 0) && ((y-ydir) <= 7)) {
+			if ((x > 0) && (board_struct->board[y-ydir][x-1] == 0)) {
+				printf("%d, %d\n", x-1, y-ydir);
+				struct gamestate *new_move = malloc(sizeof(struct gamestate));
+				memcpy(new_move, board_struct, sizeof(struct gamestate));
+				new_move->board[y-ydir][x-1] = new_move->board[y][x];
+				new_move->board[y][x] = 0;
+				append(move_list, new_move);
+			}
+			if ((x < 7) && (board_struct->board[y-ydir][x+1] == 0)) {
+				printf("%d, %d\n", x+1, y-ydir);
+				struct gamestate *new_move = malloc(sizeof(struct gamestate));
+				memcpy(new_move, board_struct, sizeof(struct gamestate));
+				new_move->board[y-ydir][x+1] = new_move->board[y][x];
+				new_move->board[y][x] = 0;
+				append(move_list, new_move);
+			}
+		}
+	}
+
+	printcaptures(board_struct, x, y, ydir, king, move_list);
+
+	return move_list;
 }
+	
+
 
 
 
 int main() {
-	struct Space *pos = (struct Space*) malloc(sizeof(struct Space));
+
+	int x, y;
 	struct gamestate *board_struct = (struct gamestate*) malloc(sizeof(struct gamestate));
 
-	int board[8][8];
-	
-	for(int i = 0; i < 8; i++) {
-		for(int j = 0; j < 8; j++) {
-			if (i <= 2) { // AI pieces
-				if ((i == 0 || i == 2)) {
-					if (j % 2 != 0) {
-						board_struct->board[i][j] = XCHECK; //global var
-					} else {
-						board_struct->board[i][j] = EMPTY; 
-					}
-				} else {
-					if (j % 2 == 0) {
-						board_struct->board[i][j] = XCHECK; //global var
-					} else {
-						board_struct->board[i][j] = EMPTY; 
-					}
+	for (int x=0; x<8; x++) {
+		for (int y=0; y<8; y++) {
+			if (y<=2) {
+				if ((y%2 == 0) != (x%2 == 0)) {
+					board_struct->board[y][x] = XCHECK;
 				}
-			}
-			if (i >= 5) { // player pieces
-				if ((i == 5 || i == 7)) {
-					if (j % 2 == 0) {
-						board_struct->board[i][j] = CHECK; //global var
-					} else {
-						board_struct->board[i][j] = EMPTY; 
-					}
-				} else {
-					if (j % 2 != 0) {
-						board_struct->board[i][j] = CHECK; //global var
-					} else {
-						board_struct->board[i][j] = EMPTY; 
-					}
+			} else if (y>=5) {
+				if ((y%2 == 0) != (x%2 == 0)) {
+					board_struct->board[y][x] = CHECK;
 				}
 			}
 		}
 	}
+	board_struct->board[4][1] = XCHECK;
+	board_struct->board[2][1] = XCHECK;
+	board_struct->board[1][2] = 0;
 
+	x = 2;
+	y = 5;
+	
+	struct LinkedList *move_list;
+	move_list = printmoves(board_struct, x, y);
 
+	printf("Printing from list:\n");
+	if (move_list->next) {
+		move_list = move_list->next;
+	}
+	while (move_list->next) {
+		printf("%d, %d\n", move_list->move->curr_x, move_list->move->curr_y);
+		move_list = move_list->next;
+	}
 
-	pos->x = 3;
-	pos->y = 2;
-	printmoves(board_struct, pos, 2);
 	return 0;
 }
